f48707ee88fd6d7984b5cc22f6c1d810bfa20e2a
Sahil Shah <ss9656484@gmail.com>
2025-01-06 23:44:21 +0530

    operator

---
+++ IncrDecrOperstor.java
@@ -0,0 +1,12 @@
+// Increment Decrement Operator
+
+public class IncrDecrOperstor {
+    public static void main(String[] args) {
+        int num1 = 80;
+        int num2 = 240;
+        num1++; // num1 = num1 + 1
+        num2--; // num2 = num2 - 1
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
+    }
+}
