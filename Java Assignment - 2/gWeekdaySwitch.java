2a9908408246426dcc1fa4ab798cf76e2fef4c6d
Sahil Shah <ss9656484@gmail.com>
2025-01-13 16:33:08 +0530

    weekday

---
+++ Java Assignment - 2/gWeekdaySwitch.java
@@ -0,0 +1,36 @@
+// Write a program to read a weekday number and print weekday name using switch statement.
+
+import java.util.Scanner;
+public class gWeekdaySwitch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter weekday number: ");
+        int weekday = sc.nextInt();
+        sc.close();
+        switch (weekday) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid weekday number");
+        }
+    }
+}
