4c04632c95e2eb58edeca26d6c31e19ff7083591
Sahil Shah <ss9656484@gmail.com>
2025-01-13 16:38:50 +0530

    sum digits

---
+++ Java Assignment - 2/jSumDigits.java
@@ -0,0 +1,17 @@
+// Write a program to find sum of digits of a number.
+
+import java.util.Scanner;
+public class jSumDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        sc.close();
+        int sum = 0;
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println("Sum of digits: " + sum);
+    }
+}
