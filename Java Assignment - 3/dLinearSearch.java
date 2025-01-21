58f862cda9578ece46821648d79b689aa96bd2f2
BeingSeight <ss9656484@gmail.com>
2025-01-21 20:36:41 +0530

    linear search

---
+++ Java Assignment - 3/dLinearSearch.java
@@ -0,0 +1,22 @@
+/*
+ WAP to perform linear search in java.
+*/
+
+class dLinearSearch {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int search = 3;
+        boolean found = false;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == search) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println("Element found");
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
