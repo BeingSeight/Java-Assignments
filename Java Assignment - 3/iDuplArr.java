5d89ab2f15b1662f6451909548e17e77faa85687
BeingSeight <ss9656484@gmail.com>
2025-01-22 10:15:13 +0530

    duplicate

---
+++ Java Assignment - 3/iDuplArr.java
@@ -0,0 +1,13 @@
+//10. Write a Java program to find duplicate values in an array of integer values.
+class DuplicateValuesInt {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,2}; 
+        
+        for(int i=0;i<arr.length;i++) { 
+            for(int j=i+1;j<arr.length;j++) { 
+                if(arr[i]==arr[j]) 
+                    System.out.println("Duplicate found: " + arr[i]); 
+            } 
+        } 
+    }
+ }
