2f79958e63733e499ef27e2531a301e29a9fb32f
BeingSeight <ss9656484@gmail.com>
2025-01-22 10:19:50 +0530

    str

---
+++ Java Assignment - 3/mStr.java
@@ -0,0 +1,10 @@
+//14. Explain String class and specify different ways to create String objects.
+// String class represents character strings and can be created using literals or the 'new' keyword.
+class mStr {
+    public static void main(String[] args) {
+        String str1 = "Hello"; // String literal
+        String str2 = new String("World"); // Using new keyword
+ 
+        System.out.println(str1 + " " + str2); 
+    }
+ }
