05dd421dd144da0ae321ffb5ae813106d58df7eb
BeingSeight <ss9656484@gmail.com>
2025-01-22 10:30:09 +0530

    lexigraphy

---
+++ Java Assignment - 3/oStrLexi.java
@@ -0,0 +1,10 @@
+//16. Write a program to compare two strings lexicographically ignoring case differences.
+class CompareStringsIgnoreCase {
+    public static void main(String[] args) {
+        String str1 = "hello"; 
+        String str2 = "HELLO"; 
+         
+        boolean isEqualIgnoreCase=str1.equalsIgnoreCase(str2);  
+        System.out.println("Strings are equal ignoring case: " + isEqualIgnoreCase);  
+    }
+ }
