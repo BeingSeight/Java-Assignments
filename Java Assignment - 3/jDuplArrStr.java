f6a2e9bfe84253881fa9b5b5ba6ba0943ba84e40
BeingSeight <ss9656484@gmail.com>
2025-01-22 10:16:18 +0530

    dupl

---
+++ Java Assignment - 3/jDuplArrStr.java
@@ -0,0 +1,14 @@
+//11. Write a Java program to find duplicate values in an array of string values.
+class DuplicateValuesString {
+    public static void main(String[] args) {
+        String[] arr = {"apple", "banana", "apple"}; 
+        
+        for(int i=0;i<arr.length;i++) { 
+            for(int j=i+1;j<arr.length;j++) { 
+                if(arr[i].equals(arr[j])) 
+                    System.out.println("Duplicate found: " + arr[i]); 
+            } 
+        } 
+    }
+ }
+ 
