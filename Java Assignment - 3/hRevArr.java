0edc616e0ae6122788a65a3173e78fe2917894b2
BeingSeight <ss9656484@gmail.com>
2025-01-22 10:14:03 +0530

    rev arr

---
+++ Java Assignment - 3/hRevArr.java
@@ -0,0 +1,10 @@
+//9. Write a Java program to reverse an array of integer values.
+class ReverseArray {
+    public static void main(String[] args) {
+       int[] arr = {1,2,3}; 
+       
+       for(int i=arr.length-1;i>=0;i--) { 
+           System.out.print(arr[i] + " "); 
+       } 
+    }
+}
