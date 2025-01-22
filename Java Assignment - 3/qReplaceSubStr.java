9b9f49a155bf0bf9ba75f1148aa1812a67d4cdb8
BeingSeight <ss9656484@gmail.com>
2025-01-22 10:32:11 +0530

    replc str

---
+++ Java Assignment - 3/qReplaceSubStr.java
@@ -0,0 +1,9 @@
+//18. Write a program to replace each substring of a given string.
+class ReplaceSubstringExample{
+    public static void main(String []args){
+       String originalStr="Hello World";  
+       String replacedStr=originalStr.replace("World", "Java");  
+         
+       System.out.println(replacedStr);  
+    } 
+ }
