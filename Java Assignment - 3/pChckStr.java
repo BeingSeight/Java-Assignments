288d9ab67b0d470e26337581612830c6d891a4d1
BeingSeight <ss9656484@gmail.com>
2025-01-22 10:31:27 +0530

    chck str

---
+++ Java Assignment - 3/pChckStr.java
@@ -0,0 +1,10 @@
+//17. Write a program to check whether two String objects contain the same data.
+class CheckStringEquality {
+    public static void main(String[] args) {
+        String str1=new String("Hello");  
+        String str2=new String("Hello");  
+         
+        boolean isEqualData=str1.equals(str2);  
+        System.out.println("Strings contain same data: " + isEqualData);  
+    }
+ }
