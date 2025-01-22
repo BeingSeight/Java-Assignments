e68139eae81c0168833d983b55b912eede846330
BeingSeight <ss9656484@gmail.com>
2025-01-22 10:48:50 +0530

    count case

---
+++ Java Assignment - 3/zaCountCase.java
@@ -0,0 +1,16 @@
+//28. Write a Java program to count number of Uppercase and Lowercase letters.
+class zaCountCase{
+    public static void main(String []args){
+      String input="Hello World";   
+      int upperCount=0;   
+      int lowerCount=0;   
+ 
+      for(char c : input.toCharArray()){   
+          if(Character.isUpperCase(c)) upperCount++;   
+          else if(Character.isLowerCase(c)) lowerCount++;   
+      }
+ 
+      System.out.printf("Uppercase count: %d Lowercase count: %d\n", upperCount, lowerCount);   
+    }   
+ }
+ 
